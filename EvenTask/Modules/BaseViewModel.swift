b12c2357739ac0ff4ebaea96cbccce033a571268
Ahmed Meguid <ameguid12@gmail.com>
2018-12-07 00:12:36 +0200

    remove dead code

--- EvenTask/Modules/BaseViewModel.swift
+++ EvenTask/Modules/BaseViewModel.swift
@@ -11,16 +11,12 @@ class BaseViewModel {
     
     var router: RouterProtocol
     var cache: CacheProtocol
-//    var location: LocationProtocol
-//    var network: NetworkProtocol
 
     var baseInteractor: BaseInteractor
     
     init(cache: CacheProtocol, router: RouterProtocol, network: NetworkProtocol, location: LocationProtocol) {
         self.router = router
         self.cache = cache
-//        self.location = location
-//        self.network = network
         self.baseInteractor = BaseInteractor(network: network, location: location, cache: cache)
     }
 }
