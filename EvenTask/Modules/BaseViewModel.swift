cf393bfbe6ed961ec0580cfe808dcd650f4631fc
Ahmed Meguid <ameguid12@gmail.com>
2018-12-06 07:37:04 +0200

    Base Architecture

--- EvenTask/Modules/BaseViewModel.swift
+++ EvenTask/Modules/BaseViewModel.swift
@@ -1,6 +1,5 @@
 //
 //  BaseViewModel.swift
-//  Archtrials
 //
 //  Created by Ahmed Meguid on 12/5/18.
 //  Copyright © 2018 Ahmed Meguid. All rights reserved.
--- EvenTask/Modules/Events/Add Event/AddEventInteractor.swift
+++ EvenTask/Modules/Events/Add Event/AddEventInteractor.swift
@@ -1,6 +1,5 @@
 //
-//  AddEventProcessor.swift
-//  Archtrials
+//  AddEventInteractor.swift
 //
 //  Created by Ahmed Meguid on 12/5/18.
 //  Copyright © 2018 Ahmed Meguid. All rights reserved.
