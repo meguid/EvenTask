9e825f86123b52ec4f8997da0a7b91f7598f292c
Ahmed Meguid <ameguid12@gmail.com>
2018-12-21 08:45:55 +0200

    caching coredata + protocol enhancements

--- EvenTask/Externals/Cache/CoreDataManager.swift
+++ EvenTask/Externals/Cache/CoreDataManager.swift
@@ -5,18 +5,62 @@
 //  Copyright © 2018 Ahmed Meguid. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
 
-class CoreDataManager: CacheProtocol {
-    func getObject<T>(_ object: T, key: CachingKey) -> T? {
-        return object
+extension CachingKey {
+    func entity() -> String {
+        return self.rawValue
     }
     
-    func saveObject<T>(_ object: T, key: CachingKey) {
-        
+    func key() -> String {
+        return self.rawValue
+    }
+    
+    func fetchRequest() -> NSFetchRequest<NSFetchRequestResult> {
+        return NSFetchRequest<NSFetchRequestResult>()
+    }
+}
+
+class CoreDataManager: NSObject, CacheProtocol {
+    
+    func getObject<T>(_ object: T, key: CachingKey) -> T? where T : Decodable, T : Encodable {
+        return getData(key: key)?[0].decode(object)
+    }
+    
+    func saveObject<T>(_ object: T, key: CachingKey) where T : Decodable, T : Encodable {
+        saveData(object.encode(), key: key)
+    }
+    
+    func getData(key: CachingKey) -> [Data]? {
+        let context = getContext()
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ImageWithFaces")
+        do{
+            return try context.fetch(fetchRequest) as? [Data]
+        } catch {
+            return nil
+        }
+    }
+    
+    func saveData(_ data: Data?, key: CachingKey) {
+        let context = getContext()
+        let entity = NSEntityDescription.entity(forEntityName: key.entity(),in: context)
+        let faceImage = NSManagedObject(entity: entity!, insertInto: context)
+        faceImage.setValue(Date(), forKey: "date")
+        faceImage.setValue(data, forKey: key.key())
+        try? context.save()
     }
     
     func removeObject(key: CachingKey) {
-        
+        let context = getContext()
+        let delete = NSBatchDeleteRequest(fetchRequest: key.fetchRequest())
+        do{
+            try context.execute(delete)
+        } catch{ }
+    }
+    
+    func getContext() -> NSManagedObjectContext{
+        let appleDelegate = UIApplication.shared.delegate as? AppDelegate
+        return (appleDelegate?.persistentContainer.viewContext)!
     }
 }
